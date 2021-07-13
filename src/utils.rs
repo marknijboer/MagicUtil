6dc193c1ba47490b6ccd72767a5f3040e01b8caa
Mark Nijboer <mark@nijboer.email>
2021-07-13 23:25:50 +0200

    * Improved output for info subcommand

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use pad::PadStr;
 
 
 pub fn print_as_json(data: HashMap<String, Option<String>>) {
@@ -13,20 +14,22 @@ pub fn print_as_lines(data: HashMap<String, Option<String>>, properties: &[&str]
     }
 }
 
-pub fn print_as_lines_with_context(data: HashMap<String, Option<String>>, properties: &[&str]) {
+pub fn print_as_lines_with_context(data: HashMap<String, Option<String>>, properties: &[&str], pad_length: Option<usize>) {
+    // Determine the longest length of the properties given.
+    let mut longest_property_length: usize = 0;
+
+    if pad_length.is_none() {
+        for property in properties {
+            if property.len() > longest_property_length {
+                longest_property_length = property.len()
+            }
+        }
+    } else {
+        longest_property_length = pad_length.unwrap();
+    }
+
     for property in properties {
         let property_value = data.get(property.to_owned()).unwrap();
-        let tabs = get_tabs_for_key(property.len());
-        println!("{}{}{}", property, tabs, property_value.clone().unwrap_or_default());
+        println!("  {} :  {}", property.pad_to_width(longest_property_length), property_value.clone().unwrap_or_default());
     }
-}
-
-fn get_tabs_for_key<'a>(len: usize) -> &'a str {
-    if len < 8 {
-        return "\t\t\t\t";
-    } else if len > 16 {
-        return "\t\t";
-    }
-
-    return "\t\t\t";
 }
